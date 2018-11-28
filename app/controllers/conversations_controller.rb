f956b5ea71d717033817efe17943a7eb60bbf320
Supermoox <mooxmk@yahoo.co.uk>
2018-11-28 18:43:46 +0200

    made vertical menu responsive

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -15,21 +15,17 @@ class ConversationsController < ApplicationController
     conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
     flash[:success] = "Your message was successfully sent!"
     redirect_to conversation_path(conversation)
-    #@search = Search.new
   end
 
   def show
     @receipts = conversation.receipts_for(current_user)
-    # mark conversation as read
     conversation.mark_as_read(current_user)
-    #@search = Search.new
   end
 
   def reply
     current_user.reply_to_conversation(conversation, message_params[:body])
     flash[:notice] = "Your reply message was successfully sent!"
     redirect_to conversation_path(conversation)
-    #@search = Search.new
   end
 
   private
