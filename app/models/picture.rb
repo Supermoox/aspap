8cc1cdffa47d02e20982a5722f1fd37d82a242a2
Supermoox <mooxmk@yahoo.co.uk>
2018-11-30 15:36:15 +0200

    s3 worked in development

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,15 +1,14 @@
 class Picture < ApplicationRecord 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100#" },
-    :default_url => "/images/:style/default_user.png",
-    :url  => ":s3_domain_url",
-    :path => "public/avatars/:id/:style_:basename.:extension",
-    :storage => :fog,
-    :fog_credentials => {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-    },
-    fog_directory: ENV["FOG_DIRECTORY"]
+  has_attached_file :image,
+   styles: { small: "64x64", medium: "100x100", large: "200x200" },
+   :s3_protocol => 'https',
+   :s3_host_name => ENV['S3_HOST_NAME'],
+   :path => ENV['S3_PATH'],
+   :storage => 's3',
+   :s3_region => ENV['AWS_REGION']
+
+   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
 
 
