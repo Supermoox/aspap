f6d416171ffd0dbc2cf8908da2a3c6015ee0cacf
Supermoox <mooxmk@yahoo.co.uk>
2018-12-09 18:44:21 +0200

    changed LIKE to ILIKE for search

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,26 +2,26 @@ class Search < ApplicationRecord
 	
 	def find_manuscripts
 		manuscripts = Manuscript.all
-		manuscripts = manuscripts.where("title LIKE ?", "%#{keywords}%") if keywords.present?
+		manuscripts = manuscripts.where("title ILIKE ?", "%#{keywords}%") if keywords.present?
 		manuscripts = manuscripts.where(journal_id: journal_id) if journal_id.present?
 		return manuscripts
 	end	
 
 	def find_users
 		users = User.all
-		users = users.where("username LIKE ?", "%#{keywords}%") if keywords.present?
+		users = users.where("username ILIKE ?", "%#{keywords}%") if keywords.present?
 		return users
 	end
 
 	def find_articles
 		articles = Article.all
-		articles = articles.where("title LIKE ?", "%#{keywords}%") if keywords.present?
+		articles = articles.where("title ILIKE ?", "%#{keywords}%") if keywords.present?
 		return articles
 	end
 
 	def find_posts
 		posts = Post.all
-		posts = posts.where("title LIKE ?", "%#{keywords}%") if keywords.present?
+		posts = posts.where("title ILIKE ?", "%#{keywords}%") if keywords.present?
 		return posts
 	end
 
