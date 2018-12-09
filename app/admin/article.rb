4b6098c3c794d5c51f0c21029ee77544e6f90fc6
Supermoox <mooxmk@yahoo.co.uk>
2018-12-09 15:00:41 +0200

    __

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Article do
-  permit_params :title, :image, :body, :user_id
+  permit_params :title, :image, :body, :directorate_id, :user_id
 end
--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Post do
-  permit_params :title, :body
+  permit_params :title, :body, :directorate_id
 end
