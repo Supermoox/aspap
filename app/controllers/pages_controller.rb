16a1ec7be0194e31f5c5ed4c95045825ddddad94
Supermoox <mooxmk@yahoo.co.uk>
2020-05-29 15:37:00 +0200

    changin display images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	def index 
 		@posts = Post.all.order("created_at").last(4)
 		@events = Event.last(4)
-		@articles = Article.all.where(approve: true).order("created_at").paginate(page: params[:page], per_page: 6)
+		@articles = Article.all.where(approve: true).order("created_at DESC").paginate(page: params[:page], per_page: 6)
 		@journals = Journal.all
 	end
 end
