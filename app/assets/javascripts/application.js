1e7daa98be3f5f6aa8c9e45ec085207b27614ae9
Supermoox <mooxmk@yahoo.co.uk>
2020-06-07 23:27:43 +0200

    jquery thing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,12 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.easy-autocomplete
 //= require rails-ujs
 //= require turbolinks
+//= require jquery.easy-autocomplete
 //= require bootstrap-sprockets
 //= require lightbox
+//= require_tree .
 
 $(document).ready(function(){
     $("#formButton").click(function(){
@@ -24,3 +25,54 @@ $(document).ready(function(){
 });
 
 
+
+
+document.addEventListener("turbolinks:load", function(){
+	$input = $("[data-behavior= 'autocomplete']")
+
+	var options = {
+
+
+		getValue: function (element) {
+				if (element.firstname == ""){
+		    	return element.title  
+				}
+		    else
+		    {    	
+		    	return element.firstname + " " + element.middlename + " " + element.surname 	   		
+		    }
+		},
+
+		url: function(phrase) {
+			return "find.json?q=" + phrase;
+		},
+
+
+    template: {
+        type: "description",
+        fields: {
+            description: "name"
+        }
+    },
+		categories: [
+			{
+				listLocation: "users",
+				header: "<h4>People</h4>",			
+			},
+			{
+				listLocation: "articles" ,
+				header: "<h4>ASPAP - Articles</h4>",
+			},
+
+		],
+		list: {
+			onChooseEvent: function() {
+				var url = $input.getSelectedItemData().url
+				Turbolinks.visit(url)
+			}
+		},
+		theme: "round"
+	}
+
+	$input.easyAutocomplete(options)
+});
