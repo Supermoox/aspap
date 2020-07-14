3f59467b9bb6e30db6c521da0e765871fb682dbc
Supermoox <mooxmk@yahoo.co.uk>
2020-07-15 01:36:48 +0200

    teting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'onemoox@gmail.com'
  
   def welcome_email
     @user = params[:user]
-    @url  = 'http://example.com/login'
+    @url  = 'http://www.aspap.org/users/sign_in'
     mail(to: @user.email, subject: 'Welcome to the ASPAP Network')
   end
 end
