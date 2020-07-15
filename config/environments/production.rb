6cf80736ed4ac7e50c2f4f62cb4cc58cbecf9568
Supermoox <mooxmk@yahoo.co.uk>
2020-07-15 20:10:45 +0200

    mm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,6 +111,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://aspap.herokuapp.com/' }
-
 end
