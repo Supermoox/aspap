834e3ff34fa5761853a0f6f74328d852d79ac23e
Supermoox <mooxmk@yahoo.co.uk>
2020-07-15 22:27:56 +0200

    mm

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   after_create :subscribe_user_to_mailing_list
   after_create :welcome_user
-  after_create :notify_user
   acts_as_voter
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -48,10 +47,6 @@ class User < ApplicationRecord
 
   def welcome_user
     UserMailer.user_welcome(self).deliver
-  end
-
-  def notify_user
-    UserNotifierMailer.send_signup_email(self).deliver
   end
 
   def subscribe_user_to_mailing_list
--- config/environment.rb
+++ config/environment.rb
@@ -10,11 +10,10 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
  :address        => 'smtp.sendgrid.net',
- :port           => '465',
+ :port           => '587',
  :authentication => :plain,
  :user_name      => ENV['SENDGRID_USERNAME'],
  :password       => ENV['SENDGRID_PASSWORD'],
  :domain         => 'heroku.com',
  :enable_startstls_auto => true
 }
-
